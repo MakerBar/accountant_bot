fb0c6471c021d0d3ec0b54251b51d77afabb0a43
Bert Hartmann <BertHartm@gmail.com>
2016-03-26 19:16:58 -0400

    apparently 'send' isn't valid for the raw http server

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var accountant_bot = new AccountantBot({
 accountant_bot.run();
 
 // silly http server to make heroku happy
-http.createServer(function (req, res) {
+http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.send('it is running\n');
+    res.end('it is running\n');
 }).listen(process.env.PORT || process.env.NODE_PORT || 5000, process.env.NODE_IP || 'localhost');
