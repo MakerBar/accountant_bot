fd622233859fe519a1998d9b630626eeec7e17f7
Bert Hartmann <BertHartm@gmail.com>
2016-07-23 13:26:13 -0400

    reconfigure the oauth flow to allow initiating from non-http sources

--- oauth.js
+++ oauth.js
@@ -3,7 +3,6 @@
 var OAuth = require('oauth');
 
 var tokensecrets = {};
-var authtokens = {};
 
 class XeroAuth {
     constructor(key, secret, host) {
@@ -19,9 +18,14 @@ class XeroAuth {
         this.hostname = host;
     }
 
-    genRequestToken(req, res) {
+    genRequestUrl() {
         var xa = this;
-        new Promise(function(resolve, reject) {
+        var access_resolve, access_reject;
+        var access_promise = new Promise(function(resolve, reject) {
+            access_resolve = resolve;
+            access_reject = reject;
+        });
+        var request_promise = new Promise(function(resolve, reject) {
             xa.oauth.getOAuthRequestToken(function(err, oAuthToken, oAuthTokenSecret, results) {
                 if (err) {
                     reject(err);
@@ -29,12 +33,19 @@ class XeroAuth {
                 if (results.error) {
                     reject(results.error);
                 }
-                tokensecrets[oAuthToken] = oAuthTokenSecret;
-                resolve({oAuthToken, oAuthTokenSecret});
+                tokensecrets[oAuthToken] = {oAuthTokenSecret, prom: {resolve: access_resolve, reject: access_reject}};
+                resolve("https://api.xero.com/oauth/Authorize?oauth_token=" + oAuthToken);
             });
-        }).then(function(tok) {
+        });
+        return [request_promise, access_promise];
+    }
+
+    genRequestToken(req, res) {
+        var xa = this;
+        let request_promise = this.genRequestUrl()[0];
+        request_promise.then(function(url) {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end("https://api.xero.com/oauth/Authorize?oauth_token=" + tok.oAuthToken);
+            res.end(url);
         }).catch(function(err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             console.log(err);
@@ -47,7 +58,7 @@ class XeroAuth {
         var token = params.oauth_token;
         var verifier = params.oauth_verifier;
         var org = params.org;
-        var request_token_secret = tokensecrets[token];
+        var request_token_secret = tokensecrets[token].oAuthTokenSecret;
         new Promise(function(resolve, reject) {
             xa.oauth.getOAuthAccessToken(token, request_token_secret, verifier,
                 function(err, oAuthAccessToken, oAuthAccessTokenSecret, results) {
@@ -57,16 +68,16 @@ class XeroAuth {
                 if (results.error) {
                     reject(results.error);
                 }
-                // currently setting the token for 'me'. Should store a user name when requesting the token
-                authtokens['me'] = {oAuthAccessToken, oAuthAccessTokenSecret, org};
-                resolve(authtokens['me']);
+                tokensecrets[token].prom.resolve({oAuthAccessToken, oAuthAccessTokenSecret, org});
+                resolve();
             });
         }).then(function(tok) {
             res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end("Verified :)\naccess token: " + tok.oAuthAccessToken);
+            res.end("Thank you for verifying");
         }).catch(function(err) {
+            tokensecrets[token].prom.reject(err);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
-            console.log(err);
+            console.log("error in verifyToken", err);
             res.end(err);
         });
     }
