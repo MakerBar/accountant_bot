f4698c137b9044c5208fdd31955e99c4be3e05cf
Bert Hartmann <BertHartm@gmail.com>
2016-07-30 19:30:37 -0400

    refactor so the conditional auth logic is right

--- accountant_bot.js
+++ accountant_bot.js
@@ -20,6 +20,17 @@ AccountantBot.prototype._onStart = function() {
     })[0];
 };
 
+function getAuthToken(ab, user) {
+    // authorize user
+    let proms = ab.xeroAuth.getAuthToken(user.id);
+    proms.request_promise.then(function(url) {
+        if (url) {
+            ab.postMessageToUser(user.name, 'please go to ' + url + ' to authorize access to Xero');
+        }
+    });
+    return proms.access_promise;
+}
+
 AccountantBot.prototype.handleMessage = function(msg) {
     let ab = this;
     var channel = this._getChannelById(msg.channel);
@@ -31,12 +42,7 @@ AccountantBot.prototype.handleMessage = function(msg) {
     }
     if (command.startsWith('balance sheet')) {
         console.log('sending balance sheet to', channel, 'for', user.name);
-        // authorize user
-        let proms = this.xeroAuth.getAuthToken(user.id);
-        proms.request_promise.then(function(url) {
-            ab.postMessageToUser(user.name, 'please go to ' + url + ' to authorize access to Xero');
-        });
-        proms.access_promise.then(function(access_obj) {
+        getAuthToken(this, user).then(function(access_obj) {
             return ab.xeroAuth.get('api.xro/2.0/Reports/BalanceSheet', access_obj);
         }).then(bs => {
             ab.postMessage(msg.channel, JSON.stringify(bs));
@@ -46,14 +52,8 @@ AccountantBot.prototype.handleMessage = function(msg) {
     }
     if (command.startsWith('oauth request')) {
         console.log('testing oauth for', user.name);
-        let proms = this.xeroAuth.getAuthToken(user.id);
-        proms.request_promise.then(function(url) {
-            if (url) {
-                ab.postMessageToUser(user.name, 'please go to ' + url + ' to authorize access to Xero');
-            }
-        });
         let path = msg.text.split(' ');
-        proms.access_promise.then(function(access_obj) {
+        getAuthToken(this, user).then(function(access_obj) {
             return ab.xeroAuth.get(path[2], access_obj);
         }).then(bs => {
             ab.postMessage(msg.channel, JSON.stringify(bs));
