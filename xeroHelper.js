06a6898aec68792a5b4eed61481dcaa56cf2e2b3
Bert Hartmann <BertHartm@gmail.com>
2016-08-07 19:36:38 -0400

    need the access_obj to call Xero...

--- xeroHelper.js
+++ xeroHelper.js
@@ -2,7 +2,7 @@
 
 function getBankTransactions(xeroAuth, access_obj) {
     // TODO: load all pages
-    return xeroAuth.get('api.xro/2.0/BankTransactions?page=1').then(function(bt) {
+    return xeroAuth.get('api.xro/2.0/BankTransactions?page=1', access_obj).then(function(bt) {
         return bt.BankTransactions;
     });
 }
