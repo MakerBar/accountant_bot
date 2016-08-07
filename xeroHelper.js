09ca00f05714df3dd8d552b92d68a169e6cf64f2
Bert Hartmann <BertHartm@gmail.com>
2016-08-07 19:59:06 -0400

    attempt a recursive promise structure to fetch all transaction pages

--- xeroHelper.js
+++ xeroHelper.js
@@ -1,10 +1,19 @@
 'use strict';
 
 function getBankTransactions(xeroAuth, access_obj) {
-    // TODO: load all pages
-    return xeroAuth.get('api.xro/2.0/BankTransactions?page=1', access_obj).then(function(bt) {
-        return bt.BankTransactions;
-    });
+    function fetchBT(page) {
+        return xeroAuth.get('api.xro/2.0/BankTransactions?page=' + String(page), access_obj).then(function(bt) {
+            let new_trans = bt.BankTransactions;
+            if (new_trans.length == 100) {
+                return fetchBT(page + 1).then(function(trans) {
+                    return new_trans.concat(trans);
+                });
+            } else {
+                return new_trans;
+            }
+        });
+    }
+    return fetchBT(1);
 }
 
 module.exports = {
